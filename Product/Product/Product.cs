12d97e3a8ebfddc67d658e5b3d1c824b18a989ab
MadhuriManjushaP <53276271+MadhuriManjushaP@users.noreply.github.com>
2019-07-25 01:36:21 +0530

    Add files via upload

---
+++ Product/Product/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Product
+    {
+        private string name;
+        private int price, quantity;
+
+        public Product(string n,int p,int q=0)
+        {
+            name = n;
+            price = p;
+            quantity = q;
+            Print();
+        }
+
+        public void Purchase(int quant)
+        {
+            quantity -= quant;
+        }
+
+        public void Sell(int quant)
+        {
+            quantity += quant;
+        }
+        public void Print()
+        {
+            Console.WriteLine("quantity {0}",quantity);
+        }
+
+    }
+}
---
+++ Product/Product/TestProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class TestProduct
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("vaseline",25,10);
+            p.Purchase(2);
+            p.Sell(5);
+            p.Print();
+        }
+
+    }
+}
