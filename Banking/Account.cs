3f77676751fc3be1b915eb275f7fe5c73c472e9f
MadhuriManjushaP <53276271+MadhuriManjushaP@users.noreply.github.com>
2019-07-25 01:33:18 +0530

    Add files via upload

---
+++ Banking/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class Account
+    {
+        private int acno;
+        private string ahname;
+        private double balance;
+
+        //methods
+
+        public void Open(int ano,string name)
+        {
+            acno = ano;
+            ahname = name;
+            balance = 0;
+
+        }
+
+        public void Deposit(double amount)
+        {   if (amount > 0)
+                balance += amount;
+            else
+                Console.WriteLine("Invalid Amount");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < balance)
+                balance -= amount;
+            else
+                Console.WriteLine("Insufficient funds");
+        }
+        //can i use 1 function to print 3 different return types?
+        public double Getbalance()
+        {   
+            return balance;
+        }
+
+        public string Accountname()
+        {
+            return ahname;
+        }
+
+        public int Accountno()
+        {
+            return acno;
+        }
+
+        
+    }
+}
---
+++ Banking/TestAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class TestAccount
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            a.Open(1,"Madhuri");
+            a.Deposit(1000);
+            a.Withdraw(200);
+            double balance=a.Getbalance();
+            string name = a.Accountname();
+            int num = a.Accountno();
+            Console.WriteLine("Account Details :");
+        Console.WriteLine ("Name:{0}" +Environment.NewLine+ "Number:{1}"+Environment.NewLine+
+            "Balance:{2}",
+                name , num , balance);  
+           // Console.WriteLine("NewLine: {0}  first line{0}  second line{0}  third line", Environment.NewLine);
+
+        }
+    }
+}
