2eba5c428d3f4bd641c218de0755f801d2713616
MadhuriManjushaP <53276271+MadhuriManjushaP@users.noreply.github.com>
2019-07-25 01:40:16 +0530

    Add files via upload

---
+++ Course/Course/Coursenew.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Coursenew
+    {
+        private string title;
+        private int duration;
+        private int price;
+
+        public void Create(string ctitle,int cduration, int cprice)
+        {
+            title = ctitle;
+            duration = cduration;
+            price = cprice;
+            Console.WriteLine("Course Duration Fee");
+            Console.WriteLine("{0},{1},{2}", title, duration, price);
+        }
+        
+        /*public void Print()
+        {
+            Console.WriteLine("Course Duration Fee");
+            Console.WriteLine("{0},{1},{2}",title, duration, price);
+        }*/
+        
+        public double Getnetfee()
+        {
+            
+            double netfee = price + (0.18 * price);
+            return netfee;
+        }
+    }
+}
---
+++ Course/Course/TestCourse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class TestCourse
+    {
+        static void Main(string[] args)
+        {
+            Coursenew c = new Coursenew();
+            c.Create("Java",45,7000);
+            c.Create("dotnet", 36, 8000);
+           // c.Print();
+            double netfee=c.Getnetfee();
+            Console.WriteLine("netfee:{0}", netfee);
+        }
+    }
+}
